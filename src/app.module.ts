eb59403d0ea95e4845147f4d075726a8d557a1a0
Nazareno Aznar Altamiranda <nazareno.aznar@gmail.com>
2026-01-03 01:17:13 -0300

    create endpoints scaffolding

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { CategoriesModule } from './categories/categories.module';
+import { BudgetsModule } from './budgets/budgets.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { CommonModule } from './common/common.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
@@ -17,6 +25,14 @@ import { ConfigModule } from '@nestjs/config';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
     }),
+    AuthModule,
+    UsersModule,
+    CategoriesModule,
+    BudgetsModule,
+    TransactionsModule,
+    DashboardModule,
+    CommonModule,
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
