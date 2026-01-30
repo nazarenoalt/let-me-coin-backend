5c1c9a3024d51760e774d975604f3913b82f15a8
Nazareno Aznar Altamiranda <nazareno.aznar@gmail.com>
2026-01-30 09:53:51 -0300

    feat: transaction entity

--- src/transactions/entities/transaction.entity.ts
+++ src/transactions/entities/transaction.entity.ts
@@ -1,1 +1,72 @@
-export class Transaction {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('transactions')
+export class Transaction {
+  @PrimaryColumn('uuid')
+  id: string;
+
+  @Column({
+    type: 'numeric',
+    precision: 18,
+    scale: 2,
+    default: 0,
+  })
+  amount: number;
+
+  @Column({
+    type: 'varchar',
+    nullable: true,
+    length: 100,
+  })
+  description: string;
+
+  @Column('varchar')
+  type: TRANSACTION_TYPE;
+
+  @Column('varchar')
+  currency: CURRENCY_TYPE;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
+
+enum TRANSACTION_TYPE {
+  Income = 'income',
+  Outome = 'outcome',
+}
+
+enum CURRENCY_TYPE {
+  // America
+  USD = 'USD',
+  CAD = 'CAD',
+
+  // Latam
+  MXN = 'MXN',
+  ARS = 'ARS',
+  UYU = 'UYU',
+  COP = 'COP',
+  BRL = 'BRL',
+  CLP = 'CLP',
+  // Europe
+  EUR = 'EUR',
+  GBP = 'GBP',
+  CHF = 'CHF',
+
+  // Asia
+  JPY = 'JPY',
+  CNY = 'CNY',
+  KRW = 'KRW',
+  INR = 'INR',
+
+  // Other
+  AUD = 'AUD',
+}
--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Transaction } from './entities/transaction.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class TransactionsService {
+  constructor(
+    @InjectRepository(Transaction)
+    private transactionRepository: Repository<Transaction>,
+  ) {}
+  
   create(createTransactionDto: CreateTransactionDto) {
     return 'This action adds a new transaction';
   }
