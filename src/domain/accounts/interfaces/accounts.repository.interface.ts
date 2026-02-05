38054e159391e7e7f5b2640c979a34b94aaf89da
Nazareno Aznar Altamiranda <nazareno.aznar@gmail.com>
2026-02-05 18:38:57 -0300

    feat: finish account repository

--- src/domain/accounts/interfaces/accounts.repository.interface.ts
+++ src/domain/accounts/interfaces/accounts.repository.interface.ts
@@ -3,13 +3,12 @@ import { Account } from '../account.entity';
 import { CreateAccountDto } from '../dto/create-account.dto';
 import { UpdateAccountDto } from '../dto/update-account.dto';
 import { PaginationDto } from '@infrastructure/modules/common/dto/pagination.dto';
+import { UpdateResult } from 'typeorm/browser';
 
 export interface IAccountsRepository {
   create(dto: CreateAccountDto): Promise<Account>;
   findAll(pagination: PaginationDto): Promise<Account[]>;
   findOne(id: string): Promise<Account | null>;
-  update(id: string, dto: UpdateAccountDto): Promise<void>;
-  updateMany(ids: string[], dto: UpdateAccountDto): Promise<void>;
+  update(id: string, dto: UpdateAccountDto): Promise<UpdateResult>;
   remove(id: string): Promise<DeleteResult>;
-  removeMany(ids: string[]): Promise<void>;
 }
--- src/infrastructure/modules/accounts/accounts.repository.ts
+++ src/infrastructure/modules/accounts/accounts.repository.ts
@@ -2,31 +2,32 @@ import { Account } from '@domain/accounts/account.entity';
 import { IAccountsRepository } from '@domain/accounts/interfaces/accounts.repository.interface';
 import { PaginationDto } from '../common/dto/pagination.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository } from 'typeorm';
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateAccountDto } from '@domain/accounts/dto/create-account.dto';
+import { UpdateAccountDto } from '@domain/accounts/dto/update-account.dto';
+import { UpdateResult } from 'typeorm/browser';
 
 @Injectable()
 export class AccountsRepository implements IAccountsRepository {
   constructor(
     @InjectRepository(Account)
-    private readonly accountRepository: Repository<Account>,
+    private readonly repository: Repository<Account>,
   ) {}
 
   async create(dto: CreateAccountDto): Promise<Account> {
-      const user = this.accountRepository.create(dto);
+    const user = this.repository.create(dto);
     try {
-      await return this.accountRepository.insert(user)
-
-    } catch(error) {
+      return await this.repository.save(user);
+    } catch (error) {
       // TODO: replace exception with global exception handler
-      throw new InternalServerErrorException(error)
+      throw new InternalServerErrorException(error);
     }
   }
-  
-  findAll({ limit = 0, offset = 0 }: PaginationDto): Promise<Account[]> {
+
+  async findAll({ limit = 0, offset = 0 }: PaginationDto): Promise<Account[]> {
     try {
-      return this.accountRepository.find({
+      return await this.repository.find({
         skip: offset,
         take: limit,
       });
@@ -36,9 +37,27 @@ export class AccountsRepository implements IAccountsRepository {
     }
   }
 
-  findOne(id: string): Promise<Account | null> {
+  async findOne(id: string): Promise<Account | null> {
     try {
-      return this.accountRepository.findOne({ where: { id } });
+      return await this.repository.findOne({ where: { id } });
+    } catch (error) {
+      // TODO: replace exception with global exception handler
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async update(id: string, dto: UpdateAccountDto): Promise<UpdateResult> {
+    try {
+      return await this.repository.update(id, dto);
+    } catch (error) {
+      // TODO: replace exception with global exception handler
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async remove(id: string): Promise<DeleteResult> {
+    try {
+      return await this.repository.delete(id);
     } catch (error) {
       // TODO: replace exception with global exception handler
       throw new InternalServerErrorException(error);
