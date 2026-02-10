78cea7fd9dde880e07d5c8a98788e443e14fd296
Nazareno Aznar Altamiranda <nazareno.aznar@gmail.com>
2026-02-10 12:41:59 -0300

    feat: create account when creating a user

--- src/infrastructure/modules/users/users.repository.ts
+++ src/infrastructure/modules/users/users.repository.ts
@@ -9,25 +9,36 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '@domain/users/user.entity';
 import { CreateUserDto, UpdateUserDto } from '@domain/users/dto';
-import { IUsersRepository } from '@domain/users/interfaces/user.repository.interface';
 import { BulkRemoveUsersDto } from '@domain/users/dto/bulk-remove-users.dto';
 import { cleanObject } from '@infrastructure/modules/common/helpers/object.helper';
 import { PaginationDto } from '../common/dto/pagination.dto';
 import { DeleteResult } from 'typeorm/browser';
-
+import { Account } from '@domain/accounts/account.entity';
+import { IUsersRepository } from '@domain/users/interfaces/user.repository.interface';
+import { CURRENCY } from '@domain/constants/currency.const';
 @Injectable()
 export class UsersRepository implements IUsersRepository {
   private readonly logger = new Logger(UsersRepository.name);
 
   constructor(
     @InjectRepository(User)
-    private readonly repository: Repository<User>,
+    private readonly userRepository: Repository<User>,
+    @InjectRepository(User)
+    private readonly accountRepository: Repository<Account>,
   ) {}
 
   async create(dto: CreateUserDto): Promise<User> {
     try {
-      const user = this.repository.create(dto);
-      return await this.repository.save(user);
+      const user = this.userRepository.create({
+        ...dto,
+        accounts: [
+          {
+            name: `${dto.firstName}'s account`,
+            currency: CURRENCY.ARS.code,
+          },
+        ],
+      });
+      return await this.userRepository.save(user);
     } catch (error) {
       this.handleException(error);
     }
@@ -37,19 +48,19 @@ export class UsersRepository implements IUsersRepository {
     offset = 0,
     limit = 50,
   }: Partial<PaginationDto>): Promise<User[]> {
-    return this.repository.find({
+    return this.userRepository.find({
       skip: offset,
       take: limit,
     });
   }
 
   async findOne(id: string) {
-    return this.repository.findOne({ where: { id } });
+    return this.userRepository.findOne({ where: { id } });
   }
 
   async update(id: string, dto: UpdateUserDto) {
     try {
-      await this.repository
+      await this.userRepository
         .createQueryBuilder()
         .update(User)
         .set({
@@ -65,7 +76,7 @@ export class UsersRepository implements IUsersRepository {
 
   async updateMany(ids: string[], dto: UpdateUserDto): Promise<void> {
     try {
-      await this.repository
+      await this.userRepository
         .createQueryBuilder()
         .update(User)
         .set({
@@ -81,7 +92,7 @@ export class UsersRepository implements IUsersRepository {
 
   async remove(id: string): Promise<DeleteResult> {
     try {
-      return await this.repository.delete({ id });
+      return await this.userRepository.delete({ id });
     } catch (error) {
       this.handleException(error);
     }
@@ -89,7 +100,7 @@ export class UsersRepository implements IUsersRepository {
 
   async removeMany({ ids }: BulkRemoveUsersDto): Promise<void> {
     try {
-      await this.repository
+      await this.userRepository
         .createQueryBuilder()
         .delete()
         .from(User)
