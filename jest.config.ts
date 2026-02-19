c4ae3c6e22f630695af23b959226ddbd8f150282
Nazareno Aznar Altamiranda <nazareno.aznar@gmail.com>
2026-02-19 08:35:13 -0300

    config: add shared path to ts and jest

--- jest.config.ts
+++ jest.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
     '^@domain/(.*)$': '<rootDir>/src/domain/$1',
     '^@application/(.*)$': '<rootDir>/src/application/$1',
     '^@infrastructure/(.*)$': '<rootDir>/src/infrastructure/$1',
-    '^@modules/(.*)$': '<rootDir>/src/modules/$1',
+    '^@shared/(.*)$': '<rootDir>/src/shared/$1',
   },
 };
 
