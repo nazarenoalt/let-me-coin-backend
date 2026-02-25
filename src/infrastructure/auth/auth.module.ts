2d15ba02854d1c278b156b19a9b08d142d2979b4
Nazareno Aznar Altamiranda <nazareno.aznar@gmail.com>
2026-02-25 18:06:39 -0300

    fix: locate correctly auth.controller in auth.module

--- src/infrastructure/auth/auth.module.ts
+++ src/infrastructure/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from '@application/auth/auth.service';
-import { AuthController } from '../../tests/infrastructure/auth/auth.controller';
+import { AuthController } from './auth.controller';
 
 @Module({
   controllers: [AuthController],
