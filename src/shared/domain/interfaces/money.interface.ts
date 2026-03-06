ff648d98b94b219c52a0b5f59d3d090dccf9ce5e
Nazareno Aznar Altamiranda <nazareno.aznar@gmail.com>
2026-03-06 20:01:57 -0300

    refactor: hange money class amount value from string to integer

--- src/shared/domain/interfaces/money.interface.ts
+++ src/shared/domain/interfaces/money.interface.ts
@@ -1,9 +1,9 @@
 import { Money } from '../value-objects/Money';
 
 export interface IMoney {
-  getAmount(): string;
-  getAbsoluteAmount(): number;
-  getCurrency(): { code: string; symbol: string };
+  getAmount(): number;
+  getFormattedAmount(): string;
+  getCurrency(): { code: string; symbol: string; exponent: number };
   add(other: Money): Money;
   subtract(other: Money): Money;
   equals(other: Money): boolean;
--- src/tests/domain/value-objects/money.spec.ts
+++ src/tests/domain/value-objects/money.spec.ts
@@ -6,36 +6,28 @@ describe('Money', () => {
   let money: Money;
 
   beforeEach(() => {
-    money = new Money('99.99', 'ARS');
+    money = new Money(9999, 'ARS');
   });
 
   describe('constructor', () => {
-    it('should throw if amount is empty', () => {
-      expect(() => new Money('', 'ARS')).toThrow(BadRequestException);
-    });
-
-    it('should throw if amount has wrong decimal places', () => {
-      expect(() => new Money('99.9', 'ARS')).toThrow(BadRequestException);
-    });
-
-    it('should throw if amount has no decimals', () => {
-      expect(() => new Money('99', 'ARS')).toThrow(BadRequestException);
+    it('should throw if amount is negative', () => {
+      expect(() => new Money(-9999, 'ARS')).toThrow(BadRequestException);
     });
 
     it('should create a Money object with valid amount', () => {
-      expect(() => new Money('99.99', 'ARS')).not.toThrow();
+      expect(() => new Money(9999, 'ARS')).not.toThrow();
     });
   });
 
   describe('getAmount()', () => {
-    it("Should return a string '99.99'", () => {
-      expect(money.getAmount()).toBe('99.99');
+    it('Should return a number 9999', () => {
+      expect(money.getAmount()).toBe(9999);
     });
   });
 
-  describe('getAbsoluteAmount()', () => {
-    it('getAbsoluteAmount() Should return a number 9999', () => {
-      expect(money.getAbsoluteAmount()).toBe(9999);
+  describe('getFormattedAmount()', () => {
+    it(`getAbsoluteAmount() Should return a string '99.99'`, () => {
+      expect(money.getFormattedAmount()).toBe('99.99');
     });
   });
 
@@ -44,51 +36,52 @@ describe('Money', () => {
     expect(money.getCurrency()).toStrictEqual({
       code: CURRENCY.ARS.code,
       symbol: CURRENCY.ARS.symbol,
+      exponent: CURRENCY.ARS.exponent,
     });
   });
 
   describe('add()', () => {
     it('Should return a new Money object with the sum of values', () => {
-      const otherMoney = new Money('10.00', 'ARS');
-      const result = new Money('109.99', 'ARS');
+      const otherMoney = new Money(1000, 'ARS');
+      const result = new Money(10999, 'ARS');
       expect(money.add(otherMoney).equals(result)).toBe(true);
     });
     it('should throw if currencies are different', () => {
-      const otherMoney = new Money('10.00', 'USD');
+      const otherMoney = new Money(1000, 'USD');
       expect(() => money.add(otherMoney)).toThrow(BadRequestException);
     });
   });
 
   describe('subtract()', () => {
     it('Should return a new Money object with the subtract of values', () => {
-      const otherMoney = new Money('10.00', 'ARS');
-      const result = new Money('89.99', 'ARS');
+      const otherMoney = new Money(1000, 'ARS');
+      const result = new Money(8999, 'ARS');
       expect(money.subtract(otherMoney).equals(result)).toBe(true);
     });
     it('should throw if result is negative', () => {
-      const otherMoney = new Money('999.99', 'ARS');
+      const otherMoney = new Money(99999, 'ARS');
       expect(() => money.subtract(otherMoney)).toThrow(BadRequestException);
     });
   });
 
   describe('equals()', () => {
-    it('Should return true if values are the same', () => {
-      const otherMoney = new Money('99.99', 'ARS');
+    it('Should return true if both amount and currency are the same', () => {
+      const otherMoney = new Money(9999, 'ARS');
       expect(money.equals(otherMoney)).toBe(true);
     });
-    it('Should return false if values are different', () => {
-      const otherMoney = new Money('10.99', 'ARS');
+    it('Should return false if both amount and currency are different', () => {
+      const otherMoney = new Money(1099, 'ARS');
       expect(money.equals(otherMoney)).toBe(false);
     });
   });
 
   describe('hasSameCurrency()', () => {
     it('Should return true if the currency is the same', () => {
-      const otherMoney = new Money('10.10', 'ARS');
+      const otherMoney = new Money(9999, 'ARS');
       expect(money.hasSameCurrency(otherMoney)).toBe(true);
     });
     it('Should return false if the currency is different', () => {
-      const otherMoney = new Money('10.10', 'USD');
+      const otherMoney = new Money(9999, 'USD');
       expect(money.hasSameCurrency(otherMoney)).toBe(false);
     });
   });
