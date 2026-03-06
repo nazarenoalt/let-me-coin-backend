d7a3eae1b782ef34c7cb5da384968aa82504bbd2
Nazareno Aznar Altamiranda <nazareno.aznar@gmail.com>
2026-03-06 17:48:46 -0300

    fix: tests

--- src/tests/infrastructure/transactions/transactions.controller.spec.ts
+++ src/tests/infrastructure/transactions/transactions.controller.spec.ts
@@ -1,6 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TransactionsController } from '@infrastructure/transactions/transactions.controller';
 import { TransactionsService } from '@application/transactions/transactions.service';
+import { TRANSACTIONS_REPOSITORY } from '@domain/transactions/interfaces/transactionsRepository.interface';
+import { TransactionsRepository } from '@infrastructure/transactions/transactions.repository';
+import { ACCOUNTS_REPOSITORY } from '@domain/accounts/interfaces/accounts.repository.interface';
+import { AccountsRepository } from '@infrastructure/accounts/accounts.repository';
+import { USERS_REPOSITORY } from '@domain/users/interfaces/user.repository.interface';
+import { UsersRepository } from '@infrastructure/users/users.repository';
 
 describe('TransactionsController', () => {
   let controller: TransactionsController;
@@ -8,7 +14,21 @@ describe('TransactionsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TransactionsController],
-      providers: [TransactionsService],
+      providers: [
+        TransactionsService,
+        {
+          provide: TRANSACTIONS_REPOSITORY,
+          useClass: TransactionsRepository,
+        },
+        {
+          provide: ACCOUNTS_REPOSITORY,
+          useClass: AccountsRepository,
+        },
+        {
+          provide: USERS_REPOSITORY,
+          useClass: UsersRepository,
+        },
+      ],
     }).compile();
 
     controller = module.get<TransactionsController>(TransactionsController);
