c9d3d7e72cbf4ae49202c9f2a5a9b875f110ee80
Nazareno Aznar Altamiranda <nazareno.aznar.altamiranda@ba.com>
2026-03-22 20:05:38 -0300

    feat: category entity

--- src/domain/categories/category.entity.ts
+++ src/domain/categories/category.entity.ts
@@ -1,1 +1,30 @@
-export class Category {}
+import { Transaction } from '@domain/transactions/transaction.entity';
+import { User } from '@domain/users/user.entity';
+import type { TcurrencyCode } from '@shared/domain/types/currencyCode.type';
+import { Column, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Category {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  title: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  color: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  emoji: string;
+
+  @ManyToOne(() => User, (user) => user.categories, {
+    onDelete: 'CASCADE',
+    orphanedRowAction: 'delete',
+  })
+  user: User;
+
+  @OneToMany(() => Transaction, (transactions) => transactions.category)
+  transactions: Transaction[];
+
+  @Column({ type: 'varchar', length: 3 })
+  currency: TcurrencyCode;
+}
